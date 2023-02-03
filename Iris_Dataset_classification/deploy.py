03515980f25b1626913f79da67fbdfdd17043bdd
Harshit <harshit.yadav190103@gmail.com>
2023-02-03 15:14:17 +0530

    First Commit

---
+++ Iris_Dataset_classification/deploy.py
@@ -0,0 +1,26 @@
+from flask import Flask , render_template, request
+import pickle
+
+app= Flask(__name__)
+
+#load the model
+
+model=pickle.load(open('saved_iris_model.sav','rb'))
+
+@app.route('/')
+
+def home():
+    result = ''
+    return render_template('index.html', **locals())
+
+@app.route('/predict', methods=['POST','GET'])
+
+def predict():
+    sepalLength = float(request.form['sepalLength']) 
+    sepalWidth = float(request.form['sepalWidth'])
+    petalLength = float(request.form['petalLength']) 
+    petalWidth = float(request.form['petalWidth']) 
+    result= model.predict([[sepalLength,sepalWidth,petalLength,petalWidth]])[0]
+    return render_template('index.html',**locals())
+if __name__ == '__main__':
+    app.run(debug=True)
